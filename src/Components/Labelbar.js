d029982b2a763ce7e3612f590e8d4bfac75f1c7d
patrick-gordon <patrickgordon42@yahoo.com>
2020-04-28 11:43:22 -0500

    good to go

--- src/Components/Labelbar.js
+++ src/Components/Labelbar.js
@@ -1,19 +1,15 @@
-import React from "react";
-import NavBar from "react-bootstrap/Navbar";
-import Nav from 'react-bootstrap/Nav'
-import './LabelBar.css'
+// import React from "react";
+// import NavBar from "react-bootstrap/Navbar";
+// import './LabelBar.css'
 
-export default function LabelBar() {
-  return (
-   <NavBar bg='dark' expand='lg'>
-    <NavBar.Brand href='/home'>GAME NAME</NavBar.Brand>
-    <NavBar.Toggle aria-controls='basic-navbar-nav' />
-    <NavBar.Collapse id='basic-navbar-nav'>
-      <Nav className='mx-auto'>
-        <Nav.Link href='/home'>Home</Nav.Link>
-        <Nav.Link href='/login'>Login</Nav.Link>
-      </Nav>
-    </NavBar.Collapse>
-   </NavBar>
-  );
-}
+// export default function LabelBar() {
+//   return (
+//    <NavBar bg='dark' expand='lg'>
+//     <NavBar.Brand href='/home'>GAME NAME</NavBar.Brand>
+//     <NavBar.Toggle aria-controls='basic-navbar-nav' />
+//     <NavBar.Collapse id='basic-navbar-nav'>
+      
+//     </NavBar.Collapse>
+//    </NavBar>
+//   );
+// }
