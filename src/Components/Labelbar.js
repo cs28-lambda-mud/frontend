183cd4b59a5b1e8de0aefd5eba5cfb39d8f05c46
patrick-gordon <patrickgordon42@yahoo.com>
2020-05-01 10:46:15 -0500

    logout function

--- src/Components/Labelbar.js
+++ src/Components/Labelbar.js
@@ -11,11 +11,11 @@ export default function LabelBar() {
     <NavBar.Toggle aria-controls='basic-navbar-nav' />
     <NavBar.Collapse id='basic-navbar-nav'>
         <Nav className='mx-auto'>
-            {localStorage.token && <Nav.Link href='/login' style={{color: 'white', marginRight: '40px'}}> Login/Register </Nav.Link>}
-            <Nav.Link to='/admin'  style={{color: 'white'}} onClick = {() => {
+            {localStorage.token && <Nav.Link href='/login' style={{color: 'white', marginRight: '40px'}}>Login/Register</Nav.Link>}
+            {localStorage.token && <Nav.Link to='/admin'  style={{color: 'white'}} onClick = {() => {
                 localStorage.removeItem('token');
-            }}>Logout</Nav.Link>
-            {localStorage.token && <Nav.Link href='/play'  style={{color: 'white', marginLeft: '40px'}}> Play </Nav.Link>}
+            }}>Logout</Nav.Link>}
+            {localStorage.token && <Nav.Link href='/play'  style={{color: 'white', marginLeft: '40px'}}>Play</Nav.Link>}
         </Nav>
     </NavBar.Collapse>
    </NavBar>
